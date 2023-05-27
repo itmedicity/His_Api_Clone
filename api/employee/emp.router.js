a2acdfb90c10868386744d1b3c4791e639831602
unknown <Administrator@MEDICITY.CO.IN>
2023-05-27 13:50:24 +0530

    User Creation

--- api/employee/emp.router.js
+++ api/employee/emp.router.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 // const { checkToken } = require("../../auth/jwtValidation");
-const { employeeDelete, employeeGetById, employeeInsert, employeeUpdate, getEmployee, login } = require('../employee/emp.controller');
+const { employeeDelete, employeeGetById, employeeInsert, employeeUpdate, employeeResetPass, getEmployee, login, searchEmployee, viewEmployee } = require('../employee/emp.controller');
 
 router.post('/login', login);
-router.post('/', employeeInsert);
-router.patch('/', employeeUpdate);
-router.get('/', getEmployee);
+router.post('/insert', employeeInsert);
+router.patch('/resetpass', employeeResetPass);
+router.patch('/update', employeeUpdate);
+router.get('/select', getEmployee);
+router.get('/view', viewEmployee);
 router.get('/:id', employeeGetById);
 router.delete('/', employeeDelete);
-
+router.post('/search', searchEmployee)
 module.exports = router;
--- api/employee/emp.service.js
+++ api/employee/emp.service.js
@@ -2,6 +2,7 @@ const pool = require('../../config/dbconfig');
 
 module.exports = {
     employeeInsert: (data, callBack) => {
+
         pool.query(
             `INSERT INTO hrm_employee 
                 (us_code,usc_name,usc_pass,usc_alias,usc_first_name,usc_active) 
@@ -23,19 +24,76 @@ module.exports = {
             }
         )
     },
-    employeeUpdate: (data, callBack) => {
+
+    EmployeeAlreadyExist: (data, callBack) => {
+        pool.query(
+            `select usc_name from hrm_employee where usc_name = ?`,
+            [
+                data.usc_name,
+            ],
+            (err, results, feilds) => {
+                if (err) {
+                    return callBack(err)
+                }
+                return callBack(null, results)
+            }
+        )
+    },
+    getEmployee: (callBack) => {
+        pool.query(
+            `SELECT * FROM medi_ellider.hrm_employee `,
+            [],
+            (error, results, feilds) => {
+                if (error) {
+                    return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    },
+
+
+    viewEmployee: (callBack) => {
+        pool.query(
+            `SELECT * FROM medi_ellider.hrm_employee order by usc_name `,
+            [],
+            (error, results, feilds) => {
+                if (error) {
+                    return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    },
+
+
+    searchEmployee: (data, callBack) => {
+        pool.query(`select * from hrm_employee where usc_name like ? and usc_alias like ? and usc_first_name like ? order by usc_name`,
+            [
+                '%' + data.usc_name + '%',
+                '%' + data.usc_alias + '%',
+                '%' + data.usc_first_name + '%',
+            ],
+            (err, results, feilds) => {
+                if (err) {
+                    return callBack(err)
+                }
+                return callBack(null, results)
+            }
+        )
+    },
+
+    employeeResetPass: (data, callBack) => {
         pool.query(
             `UPDATE hrm_employee
                 SET 
-                    us_code = ?,
-                    usc_name = ?,
+                   usc_name = ?,
                     usc_pass = ?,
                     usc_alias = ?,
                     usc_first_name = ?,
                     usc_active = ?
                 WHERE emp_slno = ?`,
             [
-                data.us_code,
                 data.usc_name,
                 data.usc_pass,
                 data.usc_alias,
@@ -51,18 +109,34 @@ module.exports = {
             }
         )
     },
-    getEmployee: (callBack) => {
+
+
+    employeeUpdate: (data, callBack) => {
         pool.query(
-            `SELECT * FROM medi_ellider.hrm_employee`,
-            [],
+            `UPDATE hrm_employee
+                SET 
+                    usc_name = ?,
+                    usc_alias = ?,
+                    usc_first_name = ?,
+                    usc_active = ?
+                WHERE emp_slno = ?`,
+            [
+                data.usc_name,
+                data.usc_alias,
+                data.usc_first_name,
+                data.usc_active,
+                data.emp_slno
+            ],
             (error, results, feilds) => {
                 if (error) {
                     return callBack(error);
                 }
                 return callBack(null, results);
             }
-        );
+        )
     },
+
+
     employeeGetById: (id, callBack) => {
         pool.query(
             `SELECT 
