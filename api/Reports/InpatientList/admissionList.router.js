22d2a122a9c187a8e18d0367e142cddec14d567d
Ajith Arjun <ajithajeesh@gmail.com>
2023-06-28 12:59:27 +0530

    Ellieder Api Patient Information Using OP Number

--- api/Reports/InpatientList/admissionList.router.js
+++ api/Reports/InpatientList/admissionList.router.js
@@ -1,10 +1,18 @@
 const { checkToken } = require('../../../auth/jwtValidation');
 const {
-    getIpAdmissionList
+    getIpAdmissionList,
+    insertTsshPat,
+    getTsshPatientDateWise,
+    deleteIPNumberFromTssh,
+    getPatientData
 } = require('./admissionList.controller');
 
 const router = require('express').Router();
 
 router.post('/getIpadmissionList', checkToken, getIpAdmissionList);
+router.post('/insertTsshPatient', checkToken, insertTsshPat);
+router.post('/getTsshPatientList', checkToken, getTsshPatientDateWise);
+router.post('/removePatiet', checkToken, deleteIPNumberFromTssh);
+router.get('/patientInfo/:id', getPatientData);
 
 module.exports = router;
