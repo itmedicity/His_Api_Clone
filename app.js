a40db7acdeb97212c77ebaa701018d12683d83ba
unknown <Administrator@MEDICITY.CO.IN>
2023-09-15 11:39:33 +0530

    changes in server

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const rolprocess = require("./api/process/rolProcess/rolProcess.router");
 //REPORT
 const gstTaxPharmacy = require("./api/Reports/GstReportTaxAndPharmacy/taxAndPharmacy.router");
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
