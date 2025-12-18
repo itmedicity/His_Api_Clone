8a2a5529d65a920bc780718ecb07cd8e673bc8f2
unknown <Administrator@MEDICITY.CO.IN>
2025-12-18 10:40:32 +0530

    store report

---
+++ api/StoreReport/StoreReport.controller.js
@@ -0,0 +1,48 @@
+const { getPurchaseMastDatas, getGrmDetails } = require('./StoreReport.service')
+
+module.exports = {
+    getPurchaseMastDatas: (req, res) => {
+        const body = req.body;
+        getPurchaseMastDatas(body, (err, results) => {
+            if (err) {
+                return res.status(200).json({
+                    success: 0,
+                    message: err
+                })
+            }
+            if (Object.keys(results).length === 0) {
+                return res.status(200).json({
+                    success: 1,
+                    message: 'No Data Found'
+                })
+            }
+            return res.status(200).json({
+                success: 2,
+                message: "Fetched Purchase Datas",
+                data: results
+            });
+        });
+    },
+    getGrmDetails: (req, res) => {
+        const body = req.body;
+        getGrmDetails(body, (err, results) => {
+            if (err) {
+                return res.status(200).json({
+                    success: 0,
+                    message: err
+                })
+            }
+            if (Object.keys(results).length === 0) {
+                return res.status(200).json({
+                    success: 1,
+                    message: 'No Data Found'
+                })
+            }
+            return res.status(200).json({
+                success: 2,
+                message: "Fetched GRN Datas",
+                data: results
+            });
+        });
+    },
+}
